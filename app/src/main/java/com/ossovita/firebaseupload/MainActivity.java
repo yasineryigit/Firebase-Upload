50142f4b9cd4429d069d96d879697fe97da32969
yasineryigit <yasineryigit737@gmail.com>
2021-04-07 00:00:16 +0300

    MainActivity designed

--- app/src/main/java/com/ossovita/firebaseupload/MainActivity.java
+++ app/src/main/java/com/ossovita/firebaseupload/MainActivity.java
@@ -1,14 +1,84 @@
 package com.ossovita.firebaseupload;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.internal.InternalTokenProvider;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE_REQUEST=1;
+    private Button mButtonUpload;
+    private TextView mTextViewShowsUploads;
+    private EditText mEditTextFileName;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
+    private Button mButtonChooseImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonUpload = findViewById(R.id.button_upload);
+        mTextViewShowsUploads = findViewById(R.id.text_view_show_uploads);
+        mEditTextFileName = findViewById(R.id.edit_text_file_name);
+        mImageView = findViewById(R.id.image_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+        mButtonChooseImage = findViewById(R.id.button_choose_image);
+
+
+
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mTextViewShowsUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICK_IMAGE_REQUEST&&resultCode==RESULT_OK&&data!=null&&data.getData()!=null){
+            mImageUri=data.getData();
+
+            Picasso.get().load(mImageUri).into(mImageView);
+            mImageView.setImageURI(mImageUri);
+
+        }
     }
 }
